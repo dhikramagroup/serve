e68d0b13111f1e48abad147c62ca3b7ab7b972d0
dhikrama creator <dhikrama.group@gmail.com>
2021-08-09 13:25:00 +0700

    static cahce added

--- src/modules/static.go
+++ src/modules/static.go
@@ -44,7 +44,11 @@ func DhikramaApp() *iris.Application {
 
 	}
 
-	app.HandleDir("/", iris.Dir("./web/public"), dirCache)
+	app.HandleDir("/js", iris.Dir("./web/public/js"), dirCache)
+	app.HandleDir("/css", iris.Dir("./web/public/css"), dirCache)
+	app.HandleDir("/icons", iris.Dir("./web/public/icons"), dirCache)
+	app.HandleDir("/images", iris.Dir("./web/public/images"), dirCache)
+	app.HandleDir("/", iris.Dir("./web/public/robots"))
 
 	// You can also register any index handler manually, order of registration does not matter:
 	// v1.Get("/static", [...custom middleware...], func(ctx iris.Context) {
