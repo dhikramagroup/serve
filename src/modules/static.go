2962ae38437afc18a2e3af5a5ffa3fa44fde1daa
dhikrama creator <dhikrama.group@gmail.com>
2021-08-09 14:20:45 +0700

    static cahce added

--- src/modules/static.go
+++ src/modules/static.go
@@ -5,7 +5,7 @@ import "github.com/kataras/iris/v12"
 func DhikramaApp() *iris.Application {
 	app := iris.New()
 
-	app.Favicon("./web/public/icons/favicon.ico")
+	app.Favicon("./web/public/favicon.ico")
 
 	// first parameter is the request path
 	// second is the system directory
@@ -45,8 +45,8 @@ func DhikramaApp() *iris.Application {
 	}
 
 	app.HandleDir("/js", iris.Dir("./web/public/js"), dirCache)
-	app.HandleDir("/css", iris.Dir("./web/public/css"), dirCache)
-	app.HandleDir("/icons", iris.Dir("./web/public/icons"), dirCache)
+	// app.HandleDir("/css", iris.Dir("./web/public/css"), dirCache)
+	// app.HandleDir("/icons", iris.Dir("./web/public/icons"), dirCache)
 	app.HandleDir("/images", iris.Dir("./web/public/images"), dirCache)
 	app.HandleDir("/", iris.Dir("./web/public/robots"))
 
