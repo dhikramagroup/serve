103f13a8e1aea341e7794e35739b4926eb8b987a
dhikrama creator <dhikrama.group@gmail.com>
2021-08-09 14:53:33 +0700

    static cahce added

--- src/modules/static.go
+++ src/modules/static.go
@@ -49,7 +49,7 @@ func DhikramaApp() *iris.Application {
 	}
 
 	app.HandleDir("/js", iris.Dir("./web/public/js"), dirCache)
-	// app.HandleDir("/css", iris.Dir("./web/public/css"), dirCache)
+	app.HandleDir("/css", iris.Dir("./web/public/css"), dirCache)
 	// app.HandleDir("/icons", iris.Dir("./web/public/icons"), dirCache)
 	app.HandleDir("/images", iris.Dir("./web/public/images"), dirCache)
 	app.HandleDir("/", iris.Dir("./web/public/robots"))
